01bfa395a547b65ed5722cc84e9dd44c799be784
smodie000 <joli1002@live.mark.se>
2018-08-30 13:53:50 +0200

    first commit

---
+++ 2018-08-30/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int  main()
+{
+	
+	
+	
+	// cout är standard utenhet, skärmen
+	// << är utmatningsoperator
+	cout << "hejsan";
+	cout << "cooooooolt";
+	cout << endl; 
+	cout << "ny rad!!!!!!!!";
+	cout << endl;
+	//programmet körs rad efter rad
+	//fungerar allt kommer vi fram 
+	//till "return 0;"
+	//heltalet 0 returneras till operativsystemet
+	return 0;	
+}
