af591b36745f187883a3feb39fa6c70e8fe1ea46
smodie000 <joli1002@live.mark.se>
2018-11-13 15:59:59 +0100

    2018-11-13

---
+++ Snake/snake.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+void Setup()
+{
+	gameOver = false;
+}
+void Draw()
+{
+	
+}
+void Input()
+{
+	
+}
+void Logic()
+{
+	
+}
+int main ()
+{
+	
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Intput();
+		Logic();
+		
+	}
+	return 0;
+}
