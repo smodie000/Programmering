796a87bb0563a69b30c3abd32a83dee56667c16b
smodie000 <joli1002@live.mark.se>
2018-11-29 14:54:01 +0100

    2018-12-29

---
+++ 2018-12-29/exempel.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	
+	for (int n=0; n < 15 ;n++) {
+		cout << n << " " ;
+		
+		if (n == 0) {
+			
+			cout << "Not a single shit" << endl;
+		}
+		
+		else if (n == 10 ) {
+			cout << "yey you surpassed 10" << endl; 
+		}
+			
+		else {
+			
+			cout << "some shit" << endl;
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ 2018-12-29/uppgift8,8.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+    int resultat=0;
+	cout << "skriv ut n " << endl;
+	cin >> n ;
+	
+	for ( int i = 1; i <= n; i++) {
+		
+		
+		if (i != n) {
+		cout << i << "+" ;
+	}
+		else {
+			cout << i;
+		}
+		resultat = resultat + i;
+	}
+	
+	cout << '=' << resultat;
+	
+	
+	
+	
+	
+	return 0;   
+}
