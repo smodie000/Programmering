a316653737e07ad8b18ba1f7c6cf087996a99790
smodie000 <joli1002@live.mark.se>
2019-01-17 14:53:50 +0100

    2019-01-17

---
+++ 2019-01-17/Age of empires.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+	
+	
+	
+	for (int x = 100; x <= 999; x++) {
+	cout << pow(x, 3);
+	for (int y = 100; x <= 999; x++){
+		cout << pow(y, 3);
+		for (int z = 100; x <= 999; x++){
+		cout << pow(z, 3);
+	}
+	}
+}
+  
+
+	
+	
+	return 0;
+	
+}
